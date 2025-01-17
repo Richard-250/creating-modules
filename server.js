b4c6dc2840219af88358fce3a1001a191e1cfcac
cyubahiro Richard <cyubahirorichard250@gmail.com>
2025-01-17 02:18:46 -0800

    adding readFile module

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var http = require('http');
+var fs = require('fs')
 var dt = require('./mymodules')
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write("The date and time are currently: " + dt.myDateTime()),
-    res.end('Hello ICT World!');
-    console.log(`server is listening to port http://localhost:8080`)
+    fs.readFile('./file.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write("The date and time are currently: " + dt.myDateTime());
+        res.write(data);
+        res.write(req.url)
+        console.log(`server is listening to port http://localhost:8080`)
+    })
+    
 }).listen(8080);
 
-          
